d0f46725fbc28e29ea5190888cdf48874bb74bf7
Vinicius Nunes <viniciusanm2112@gmail.com>
2024-02-08 20:55:54 -0300

    model evaluation completed

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-""" from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
-from textSummarizer.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline """
+from textSummarizer.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 from textSummarizer.logging import logger
 
 
@@ -31,7 +31,7 @@ except Exception as e:
 
 
 
-""" STAGE_NAME = "Data Transformation stage"
+STAGE_NAME = "Data Transformation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_transformation = DataTransformationTrainingPipeline()
@@ -66,4 +66,4 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
-        raise e """
+        raise e
